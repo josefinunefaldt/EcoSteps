5d5f02befa00d96fa83c126dc7324f1e44e76d13
josefinunefaldt <josefin6@hotmail.com>
2024-12-19 14:03:06 +0100

    Feat: navbar Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import Navbar from "./components/navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
+
+export default App;
