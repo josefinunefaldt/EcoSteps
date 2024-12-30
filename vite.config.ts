624167b001a7e529824b49087beaad1b2b1317c0
josefinunefaldt <josefin6@hotmail.com>
2024-12-30 11:12:00 +0100

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
-  base: "./ecosteps",
+  base: "./EcoSteps",
 });
